121a8442363eadc514b29080a13e3148a62eeba7
anshup <anshup@INanshup.local>
2015-08-31 02:35:13 +0530

    Added the view for calculator, spec may need mockito, halting it for a while, though added the test file for specs

--- src/com/tw/calculator/CalculatorView.java
+++ src/com/tw/calculator/CalculatorView.java
@@ -1,7 +1,46 @@
 package com.tw.calculator;
 
-/**
- * Created by anshup on 31/08/15.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalculatorView {
+    static Calculator calculator = new Calculator();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine;
+        System.out.print(">");
+        inputLine = br.readLine().toLowerCase();
+        while(true) {
+            separateCommandAndOperandAndCallCalculator(inputLine);
+            System.out.println("");
+            System.out.print(">");
+            inputLine = br.readLine().toLowerCase();
+        }
+    }
+
+    public static void separateCommandAndOperandAndCallCalculator(String inputLine) {
+        String command;
+        Double operand = 0.0;
+        try {
+            String commandAndOperandArray[] = inputLine.split(" ");
+            command = commandAndOperandArray[0];
+            try {
+                operand = Double.parseDouble(commandAndOperandArray[1]);
+            }
+            catch(NumberFormatException nfe) {
+                System.out.println("Error! Provide a valid number");
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException aioobe) {
+            command = inputLine;
+        }
+        if (!(calculator.isSupported(command))){
+            System.out.println("Unsupported Operation");
+        }
+        else
+        {
+            calculator.call(command, operand);
+        }
+    }
 }
--- test/com/tw/calculator/CalculatorViewTest.java
+++ test/com/tw/calculator/CalculatorViewTest.java
@@ -1,7 +1,5 @@
 package com.tw.calculator;
 
-/**
- * Created by anshup on 31/08/15.
- */
 public class CalculatorViewTest {
+
 }
